f762f5ecafa391f77414f883e0b2e2222a6fc3b3
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-09-22 14:53:44 +0530

    Sidebar and navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
-import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" Component={<Home />} />
-        <Route path="/video/:gatogaryId/:videoId" Component={<Video />} />
-      </Routes>
+      <Home />
+      <Video />
     </>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,12 +17,54 @@ import cameron from "../assets/cameron.png";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      heloooo
-      {/* <div className="shortcut-link">
+      <div className="shortcut-link">
         <div className="side-link">
-          <img src={home} alt="" />
+          <img src={home} alt="" /> <p>Home</p>
         </div>
-      </div> */}
+        <div className="side-link">
+          <img src={automobiles} alt="" /> <p>automobiles</p>
+        </div>
+        <div className="side-link">
+          <img src={blogs} alt="" /> <p>blogs</p>
+        </div>
+        <div className="side-link">
+          <img src={games} alt="" /> <p>games</p>
+        </div>
+        <div className="side-link">
+          <img src={sports} alt="" /> <p>sports</p>
+        </div>
+        <div className="side-link">
+          <img src={entertainments} alt="" /> <p>entertainments</p>
+        </div>
+        <div className="side-link">
+          <img src={tech} alt="" /> <p>tech</p>
+        </div>
+        <div className="side-link">
+          <img src={music} alt="" /> <p>music</p>
+        </div>
+        <div className="side-link">
+          <img src={news} alt="" /> <p>news</p>
+        </div>
+        <hr />
+        <div className="subscribed-list">
+          <h3>SUBSCRIBED</h3>
+          <div className="side-link">
+            <img src={tom} alt="" /> <p>salmanfaris</p>
+          </div>
+          <div className="side-link">
+            <img src={simon} alt="" /> <p>unnikrshnan</p>
+          </div>
+          <div className="side-link">
+            <img src={megan} alt="" /> <p>saranya</p>
+          </div>
+          <div className="side-link">
+            <img src={jack} alt="" /> <p>archana</p>
+          </div>
+          <div className="side-link">
+            <img src={cameron} alt="" /> <p>mr beans</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
