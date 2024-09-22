975e61f5367b6c8f4d46e9071ad62664d4116f6b
salmanulfarisvnb <salmanulfarisp9946@gmail.com>
2024-09-22 13:48:37 +0530

    route some issue

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import home from "../assets/home.png";
+import automobiles from "../assets/automobiles.png";
+import blogs from "../assets/blogs.png";
+import games from "../assets/game_icon.png";
+import sports from "../assets/sports.png";
+import entertainments from "../assets/entertainment.png";
+import tech from "../assets/tech.png";
+import music from "../assets/music.png";
+import news from "../assets/news.png";
+import jack from "../assets/jack.png";
+import simon from "../assets/simon.png";
+import megan from "../assets/megan.png";
+import tom from "../assets/tom.png";
+import cameron from "../assets/cameron.png";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      heloooo
+      {/* <div className="shortcut-link">
+        <div className="side-link">
+          <img src={home} alt="" />
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Sidebar;
